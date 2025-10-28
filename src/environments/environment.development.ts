a132de43c957a94e19afa89c6a211586294d6242
AFOLABI GMATT MATTHEW <gmattworld@gmail.com>
2025-10-28 19:01:00 +0100

    Update environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -38,7 +38,7 @@ export const environment = {
   },
 
   // AltarBase Config
-  API_BASE_URL: 'https://edmiulioah2abyv3jtnc6jod4q0liybk.lambda-url.eu-central-1.on.aws/api',
+  API_BASE_URL: 'http://127.0.0.1:5074/api',
   TENANT_PUBLIC_KEY: '01K5MVZDTVDGJEBRH9Y5FMSRG6',
   TENANT_SECRET_KEY: 'af347116-d602-4303-9cf8-7f3c0785f526',
 };
